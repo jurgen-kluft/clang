83c378842a4fb214a4c8e7a92835378a03168fb3
Jurgen <jurgen@virtuos.com.cn>
2013-10-26 15:01:49 +0800

    *:

---
+++ source/main/cpp/x_llfw_allocator.cpp
@@ -0,0 +1,19 @@
+namespace xcore
+{
+	namespace atomic
+	{
+
+		/** Lockless forward allocator
+
+		We have to set a minimum and maximum allocation size:
+		- Minimum = 32 bytes
+		- Maximum = 32 * 32 (bits) = 1024 bytes
+
+
+		**/
+
+
+
+
+	}
+}
