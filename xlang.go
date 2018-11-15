772c30d920042e5861a6cb6d751daa4bf437fcef
Jurgen Kluft <jurgen.kluft@gmail.com>
2018-11-15 20:10:20 +0800

    * adjusted for xcode/tundra

--- xlang.go
+++ xlang.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/jurgen-kluft/xlang/package"
 	"github.com/jurgen-kluft/xcode"
+	"github.com/jurgen-kluft/xlang/package"
 )
 
 func main() {
+	xcode.Init()
 	xcode.Generate(xlang.GetPackage())
 }
